0a3bea5df80b1b88dfb5caf26d1d41a83d718b9d
Sagi <sagi.kim5@gmail.com>
2022-03-27 18:33:04 -0400

    release version

--- clean_test_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/tcsh
-
-if ( "$1" != "" ) then
-    set testDir = "$1"
-else
-    set testDir = "tests"
-endif
-
-rm ${testDir}/*.am ${testDir}/*.ob ${testDir}/*.ent ${testDir}/*.ext ${testDir}/*.err
